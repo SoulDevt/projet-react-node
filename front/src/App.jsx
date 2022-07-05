d096022fe764eef00cc3a74681202d747d21bfba
SoulTheDevt <souleymane.gueye.pro@gmail.com>
2022-07-05 11:15:24 +0200

    add routes.js

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,11 @@
+//Librairies
 import { React, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import routes from './routes';
+import { Link } from "react-router-dom"
 
 //Components
-
 import { Routes, Route } from "react-router-dom";
 import Home from './Components/Home';
 import Test1 from './Components/Test1';
@@ -14,11 +16,15 @@ function App() {
   return (
     <div className="App">
         <Routes>
-            <Route path ='/' element={<Home/>}></Route>
-            <Route path ='/test1' element={<Test1/>}></Route>
-            <Route path ='/test2' element={<Test2/>}></Route>
+            <Route path ={routes.HOME} element={<Home/>}></Route>
+            <Route path ={routes.test1} element={<Test1/>}></Route>
+            <Route path ={routes.test2} element={<Test2/>}></Route>
             <Route path ='*' element={<NotFound/>}></Route>
         </Routes>
+
+        <Link to={routes.HOME}>Home</Link>
+        <Link to={routes.test1} style={{marginLeft: '5px'}}>Test1</Link>
+        <Link to={routes.test2} style={{marginLeft: '5px'}}>Test2</Link>
     </div>
   )
 }
---
+++ front/src/routes.js
@@ -0,0 +1,7 @@
+const routes = {
+    HOME: "/",
+    test1: "/test1",
+    test2: "/test2"
+}
+
+export default routes;
