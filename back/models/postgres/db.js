9f8de634a23e387189291e32880ccec8e301d97f
YungMides <m1d3s@protonmail.com>
2022-07-27 01:01:01 +0000

    Front

--- back/models/postgres/db.js
+++ back/models/postgres/db.js
@@ -1,16 +1,15 @@
 const Sequelize = require("sequelize");
 
 const connection = new Sequelize(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
+    useNewUrlParser: true,
 });
-
 connection
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
+    .authenticate()
+    .then(() => {
+        console.log("Connection has been established successfully.");
+    })
+    .catch((err) => {
+        console.error("Unable to connect to the database:", err);
+    });
 
 module.exports = connection;
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,49 +1,61 @@
 //Librairies
-import { React, useState, useEffect } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import routes from './routes';
-import { Link } from "react-router-dom"
-import jwt_decode from 'jwt-decode';
-import NavLogged from './Components/NavLogged';
-import NavNotLogged from './Components/NavNotLogged';
+import { React, useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import routes from "./routes";
+import { Link } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import NavLogged from "./Components/NavLogged";
+import NavNotLogged from "./Components/NavNotLogged";
 
 //Components
 import { Routes, Route } from "react-router-dom";
-import Home from './Components/Home';
-import Login from './Components/Login';
-import Register from './Components/Register';
-import NotFound from './Components/NotFound'
-import Logout from './Components/Logout';
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import NotFound from "./Components/NotFound";
+import Logout from "./Components/Logout";
+import EsgiNavbar from "./Components/EsgiNavbar";
+import Loader from "./Components/Loader";
+
 function App() {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
+  const [isLogged, setIsLogged] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  
   useEffect(() => {
-    const thereIsToken = localStorage.getItem('JWT');
-    if (thereIsToken != null) {
-      const token = jwt_decode(localStorage.getItem('JWT'));
-      setName(token.name);
-    } else {
-      console.log('NO');
-    }
-
-  })
-
-
+    setIsLoading(true);
+    const thereIsToken = localStorage.getItem("JWT");
+    if (thereIsToken == null)
+      return  
+    const token = jwt_decode(localStorage.getItem("JWT"));
+    setName(token.name);
+    setIsLogged(true);
+    setIsLoading(false);
+  }, []);
+  
+  useEffect(() => {
+    if(!isLoading)
+      return
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, [isLoading]);
   return (
     <div className="App">
-      {name ? <NavNotLogged /> : <NavLogged />}
-      <Routes>
-        <Route
-          path={routes.HOME}
-          element={<Home name={name} />}
-        />
-        <Route path={routes.login} element={<Login />}></Route>
-        <Route path={routes.register} element={<Register />}></Route>
-        <Route path={routes.logout} element={<Logout />}></Route>
-        <Route path='*' element={<NotFound />}></Route>
-      </Routes>
+      <EsgiNavbar isLogged={isLogged} name={name}/>
+      {isLoading ? <Loader /> : 
+        <Routes>
+          <Route path={routes.HOME} element={<Home name={name} isLogged={isLogged} />} />
+          <Route path={routes.login} element={<Login />}></Route>
+          <Route path={routes.register} element={<Register />}></Route>
+          <Route path={routes.logout} element={<Logout />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      }
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
